d596fc9b0121b2f872b1f7123b940923bdc03f9f
Dheeraj S <dheerajstvpm@gmail.com>
2024-02-10 23:06:40 +0530

    changes

---
+++ app.ts
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const logger = require('morgan')
+app.use(logger('dev'))
+const cors = require('cors')
+app.use(cors())
+
+const api = require('./api');
+app.use(express.json());
+app.use('/api', api)
+
+app.use('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+//404 page
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/404_page.html')
+})
+
+app.listen(4000, (req, res) => {
+    console.log('server is running in port 4000');
+})
