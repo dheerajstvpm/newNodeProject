8ecd17f7f8b35b4e5d89a7e3ccfb42fb188b76d3
Dheeraj S <dheerajstvpm@gmail.com>
2024-02-10 23:54:17 +0530

    changes

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.send("From API route")
+    res.send({text:"From API route"})
 });
 
 export default router;
